de55488ad1c3b1802f62612a2b46f0fd95c39ad3
Osama Makhlouph <30751973+Osama-Makhlouph@users.noreply.github.com>
2022-07-08 22:07:36 +0200

    Added cart-context-processor, created orders-app, its models and added it to admin-site

--- cart/cart.py
+++ cart/cart.py
@@ -69,7 +69,7 @@ class Cart(object):
         return sum(item['quantity'] for item in self.cart.values())
 
     def get_total_price(self):
-        return sum(item['price'] * item['quantity'] for item in self.cart.values())
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
 
     def clear(self):
         # remove cart from session
